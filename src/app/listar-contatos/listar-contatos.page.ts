0b5b379bfedcddcfbdef03ed167ee4486d98f4e9
Cauã silva <caua07542@gmail.com>
2025-11-04 21:01:35 -0300

    commit4

--- src/app/listar-contatos/listar-contatos.page.ts
+++ src/app/listar-contatos/listar-contatos.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-listar-contatos',
   templateUrl: './listar-contatos.page.html',
   styleUrls: ['./listar-contatos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ListarContatosPage implements OnInit {
 
